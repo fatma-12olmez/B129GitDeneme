91a99c117b6bea792dff12170021a78794482f0b
fatma-12olmez <olmezfatma94@gmail.com>
2023-02-26 14:57:25 +0100

    v6

--- src/main/java/Deneme.java
+++ src/main/java/Deneme.java
@@ -4,6 +4,7 @@ public class Deneme {
         System.out.println("Merhaba");
         System.out.println("Dunya");
         System.out.println("B129");
+        //git
     }
 }
 //Restore --> Tamamen Working space temizler
