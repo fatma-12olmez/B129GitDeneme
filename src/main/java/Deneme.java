315d7b581b656ee0ff3c05bfc01638a549818f45
fatma-12olmez <olmezfatma94@gmail.com>
2023-02-26 14:39:12 +0100

    v3

--- src/main/java/Deneme.java
+++ src/main/java/Deneme.java
@@ -2,8 +2,5 @@ public class Deneme {
     public static void main(String[] args) {
 
         System.out.println("Merhaba");
-        System.out.println("Dunya");
     }
 }
-//Restore --> Tamamen Working space temizler
-//Stash --> Hafizaya alir
